5dab16410072b8ffd7c251526471b67c27d12fae
Pawel Glowacki <eros.z@poczta.fm>
2016-12-23 19:40:22 +0100

    triangleArea

---
+++ scripts.js
@@ -0,0 +1,4 @@
+var a = prompt("Podaj długość podstawy trójkąta")
+var h = prompt("Podaj wysokość trójkąta")
+var triangleArea = ((a * h) / 2)
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
