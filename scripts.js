1d93f53019ef7d77789138f404271f3406ac1e22
Pawel Glowacki <eros.z@poczta.fm>
2016-12-26 17:09:20 +0100

    changes made

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
-var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach."
-var newText = "Zielone słonie"
-var animal = newText.toUpperCase()
-var textReplace = text.replace('Papugi', animal)
-var textReplacePart = textReplace.slice(0, 45)
+var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.";
+var newText = "Zielone słonie";
+var animal = newText.toUpperCase();
+var textReplace = text.replace(text.split(" ", 1), animal);
+function halfText() {
+    return textReplace.length/2;
+}
+var textReplacePart = textReplace.slice(0, halfText());
+
 console.log(textReplacePart);
