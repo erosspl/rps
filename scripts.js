0ae9b850b26a586ff2deebb5bee0ce4cd9b9d933
Pawel Glowacki <eros.z@poczta.fm>
2016-12-26 18:41:33 +0100

    solved

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
-function getTriangleArea(a, h) {
-  var a = prompt("Podaj długość podstawy trójkąta")
-  var h = prompt("Podaj wysokość trójkąta")
-  if ( a<=0 || h<=0 ) {
-    alert("Nieprawidłowe dane");
-	}
-  else {
-    return a * h / 2;
-  }
-};
-console.log( getTriangleArea() );
+function init() {
+	var a = prompt("Podaj długość podstawy trójkąta");
+	var h = prompt("Podaj wysokość trójkąta");
+ 	return getTriangleArea( a , h );
+}
+
+function getTriangleArea( a , h ) {
+	if ( a <= 0 || h <= 0 ) {
+    	console.log("Nieprawidłowe dane");
+    	return alert("Nieprawidłowe dane");
+    }
+	return a * h / 2;
+}
+
+console.log(init());
